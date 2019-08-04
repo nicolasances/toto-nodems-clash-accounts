52d44d152b1c2c8de7bf24b335fdc1a0f5f5fc61
nicolasances <nicolas.matteazzi@gmail.com>
2019-08-04 11:41:19 +0200

    user in body

--- dlg/PostAccount.js
+++ dlg/PostAccount.js
@@ -7,7 +7,7 @@ var MongoClient = mongo.MongoClient;
 /**
  * Posts a new account. Will save it as "not current"
  * Requires:
- *  - req.query.user               : the email of the user
+ *  - user                         : the email of the user
  *  - tag                          : of the account
  *  - name                         : name of the account 
  */
@@ -15,10 +15,10 @@ exports.do = (req) => {
 
     return new Promise((success, failure) => {
 
-        let email = req.query.user;
+        let email = req.bodys.user;
 
         // VAlidation 
-        if (!email) { failure({ code: 400, message: '"user" is a required query param' }); return; }
+        if (!email) { failure({ code: 400, message: '"user" is a mandatory field' }); return; }
         if (!req.body.tag) { failure({ code: 400, message: '"tag" is a mandatory field' }); return; }
         if (!req.body.name) { failure({ code: 400, message: '"name" is a mandatory field' }); return; }
 
